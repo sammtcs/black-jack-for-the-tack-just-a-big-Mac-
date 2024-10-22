468587e73df3c6f2c52e01682797a31ebd1d4df9
sammtcs <samm-farmington@thecoderschool.com>
2024-10-22 22:56:54 +0000

    i made a little stuff for blackjack

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+
+playerhand = random.randint = + random.randint(1,10)
+dealerhand = random.randint(1,10)
+print(dealerhand)
+while dealerhand < 17:
+    dealerhand =+ random.randit(1,10)
+    print(dealerhand)
+
+
+
+
