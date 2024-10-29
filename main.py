d622f899a9c26dd4f7e2a02e9a64e14eb8207911
sammtcs <samm-farmington@thecoderschool.com>
2024-10-29 22:58:25 +0000

    i addid more stuff im 12 no judge

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import random
 
-playerhand = random.randint = + random.randint(1,10)
-dealerhand = random.randint(1,10)
+playerhand = random.(randint) + random.(randint)(1,10)
+dealerhand = random.(randint)(1,10)
 print(dealerhand)
 while dealerhand < 17:
     dealerhand =+ random.randit(1,10)
     print(dealerhand)
 
+gamecondition = 2
 
 
+if dealerhand >21:
+    print("you win")
+    gamecondition = 0
+elif dealhand == 21:
+    print("you lose")
+    gamecondition = 1
+if gamecondition >1:
+    print(f"your current hand value is: {playerhand}")
+    hitOrstand = input("hit or stand")
+
+    while hitOrstand == ('hit'):
+        playerhand =+ random.randint(1,10)
 
